0e7b16f33e4e6ccb8efe3dd16fb248c78998bf9b
TamakiH <95512296+TamakiH@users.noreply.github.com>
2022-01-08 11:56:00 +0900

    Created a button and changed the country

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+function getCovidData() {
+  fetch('https://covid19-api.com/country?name=Japan&format=json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
