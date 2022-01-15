5cbda4441bc6ebce52333ebe12156979766a4c4f
TamakiH <95512296+TamakiH@users.noreply.github.com>
2022-01-15 11:58:20 +0900

    display data

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Hello World!");
+
+//Function to get covid API data by country and display on HTML
 async function getCovidData() {
   var countryName=document.getElementById("countryName").value;
   let response = await fetch('https://covid19-api.com/country?name='+countryName+'&format=json');
@@ -7,13 +9,13 @@ async function getCovidData() {
 
   if (response.status === 200) {
       let data = await response.text();
-      console.log(data);
-      console.log(typeof(data));
-      // handle data
-  }
-  //console.log(response);
-  // fetch('https://covid19-api.com/country?name='+countryName+'&format=json')
-  // .then(response => response.json())
-  // .then(data => console.log(data));
+      var result = data.substring(1, data.length-1);
+      const data_json = JSON.parse(result);
+      console.log(data_json);
+      console.log(typeof(data_json));
+      document.getElementById("cases").innerHTML = "Cases: " +data_json.confirmed;
+      document.getElementById("critical").innerHTML = "Critical: " +data_json.critical;
+      document.getElementById("death").innerHTML = "Death: " +data_json.deaths;
 
+  }
 }
