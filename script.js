1d634ed2a2d88ebc1faa6b6310efa31b4afa93c0
TamakiH <95512296+TamakiH@users.noreply.github.com>
2022-01-29 12:03:13 +0900

    added datas for graph

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ async function getCovidData() {
         document.getElementById("cases").innerHTML = "Cases: " +data_json.confirmed;
         document.getElementById("critical").innerHTML = "Critical: " +data_json.critical;
         document.getElementById("death").innerHTML = "Death: " +data_json.deaths;
+        graph(data_json.confirmed, data_json.recovered, data_json.critical, data_json.deaths);
       }
       //catch countries that do not exist
       catch (SyntaxError) {
@@ -29,31 +30,26 @@ async function getCovidData() {
       }
   }
   else{
-    console.log(response.status)
+    console.log(response.status);
   }
-  graph();
-}
+ } 
 
-function graph(){
+function graph(confirmed, recovered, critical, deaths){
   const ctx = document.getElementById("myChart");
   const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: ["confirmed", "recovered", "critical", "deaths"],
           datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
+              label: ["Confirmed”, “Recovered”, “Critical”, “Deaths"],
+              data: [confirmed, recovered, critical, deaths],
               backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
                   'rgba(255, 206, 86, 0.2)',
                   'rgba(75, 192, 192, 0.2)',
                   'rgba(153, 102, 255, 0.2)',
                   'rgba(255, 159, 64, 0.2)'
               ],
               borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
                   'rgba(255, 206, 86, 1)',
                   'rgba(75, 192, 192, 1)',
                   'rgba(153, 102, 255, 1)',
